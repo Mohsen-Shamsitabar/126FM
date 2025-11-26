18b6bcbb71a0f422f49ca30b576906e0dde9a232
AmirManshadi <amirmanshadi.code@gmail.com>
2025-11-26 13:13:19 +0330

    refactor: clean up code formatting and remove unused components
    - Removed the AnimatedText component from App.tsx.
    - Updated formatting in tsconfig files for better readability.
    - Ensured consistent use of arrays in tsconfig options.
    - Added missing newlines at the end of several files.

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ A static HTML single-page application (SPA) game.
 
 ## Design
 
-[Figma Design File](https://www.figma.com/design/HGdgGU7xh1AwxIUezGFqAz/126.0-FM?node-id=20-8049&t=McjWZxo20Qbo0DBk-1)
+[Figma Design File](https://www.figma.com/design/HGdgGU7xh1AwxIUezGFqAz/126.0-FM)
 
 ## About
 
--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,7 @@
-import AnimatedText from "@/components/AnimatedText";
-
 const App = () => {
   return (
     <>
       <h1>126FM</h1>
-
-      <AnimatedText
-        text="Lorem ipsum dolor sit, amet consectetur adipisicing elit. Mollitia, eos earum voluptate perferendis corrupti necessitatibus repudiandae quae voluptatem magni doloremque culpa ipsum ullam unde. Error similique voluptatem adipisci natus animi."
-        interval={50}
-      />
     </>
   );
 };
