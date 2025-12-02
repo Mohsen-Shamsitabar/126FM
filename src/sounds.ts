b9686e6d6a8bece6cbbf1149d2932c3b1d244f41
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-12-02 18:58:12 +0330

    reconfigure build

--- src/sounds.ts
+++ src/sounds.ts
@@ -2,22 +2,22 @@ import { Howl } from "howler";
 
 const sounds = {
   radioClick: new Howl({
-    src: "sounds/radio-click.mp3",
+    src: "/sounds/radio-click.mp3",
     format: "mp3",
     volume: 0.75,
   }),
   wrongAnswer: new Howl({
-    src: "sounds/wrong-answer.mp3",
+    src: "/sounds/wrong-answer.mp3",
     format: "mp3",
     volume: 0.75,
   }),
   keyboardPress: new Howl({
-    src: "sounds/keyboard-press.mp3",
+    src: "/sounds/keyboard-press.mp3",
     format: "mp3",
     volume: 0.5,
   }),
   correctAnswer: new Howl({
-    src: "sounds/correct-answer.mp3",
+    src: "/sounds/correct-answer.mp3",
     format: "mp3",
     volume: 0.75,
   }),
@@ -25,13 +25,13 @@ const sounds = {
   // ========= BACKGROUND ========= //
 
   mainMenu: new Howl({
-    src: "sounds/jay-varton_my-kind-of-illusion.mp3",
+    src: "/sounds/jay-varton_my-kind-of-illusion.mp3",
     format: "mp3",
     volume: 0.3,
     loop: true,
   }),
   radioStatic: new Howl({
-    src: "sounds/radio-static.mp3",
+    src: "/sounds/radio-static.mp3",
     format: "mp3",
     volume: 0.1,
     loop: true,
