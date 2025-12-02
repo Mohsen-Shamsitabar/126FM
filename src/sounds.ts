f43fa904890a0603b67d488c2c6b2880f012c52a
Mohsen Shamsitabar <shamsitabar.mohsen@gmail.com>
2025-12-02 18:34:20 +0330

    refactor assets

--- src/sounds.ts
+++ src/sounds.ts
@@ -2,41 +2,36 @@ import { Howl } from "howler";
 
 const sounds = {
   radioClick: new Howl({
-    src: "src/assets/audio/radio-click.mp3",
+    src: "/sounds/radio-click.mp3",
     format: "mp3",
     volume: 0.75,
   }),
   wrongAnswer: new Howl({
-    src: "src/assets/audio/wrong-answer.mp3",
+    src: "/sounds/wrong-answer.mp3",
     format: "mp3",
     volume: 0.75,
   }),
   keyboardPress: new Howl({
-    src: "src/assets/audio/keyboard-press.mp3",
+    src: "/sounds/keyboard-press.mp3",
     format: "mp3",
     volume: 0.5,
   }),
   correctAnswer: new Howl({
-    src: "src/assets/audio/correct-answer.mp3",
+    src: "/sounds/correct-answer.mp3",
     format: "mp3",
     volume: 0.75,
   }),
 
-  // ========= PAGES ========= //
+  // ========= BACKGROUND ========= //
 
-  channel3: new Howl({
-    src: "src/assets/audio/communication-1.mp3",
-    format: "mp3",
-    volume: 0.3,
-  }),
   mainMenu: new Howl({
-    src: "src/assets/audio/jay-varton_my-kind-of-illusion.mp3",
+    src: "/sounds/jay-varton_my-kind-of-illusion.mp3",
     format: "mp3",
     volume: 0.3,
     loop: true,
   }),
   radioStatic: new Howl({
-    src: "src/assets/audio/radio-static.mp3",
+    src: "/sounds/radio-static.mp3",
     format: "mp3",
     volume: 0.1,
     loop: true,
